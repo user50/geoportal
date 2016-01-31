0581541064ae510f8f9afd529d03862d8f1cea58
EvgenLozov <jmen7070@gmail.com>
2016-01-31 12:41:17 +0200

    TokenController has been done

---
+++ src/main/java/org/w2fc/geoportal/ws/http/TokenController.java
@@ -0,0 +1,58 @@
+package org.w2fc.geoportal.ws.http;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.w2fc.geoportal.dao.GeoUserDao;
+import org.w2fc.geoportal.domain.GeoUser;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author Yevhen
+ */
+@Controller
+@RequestMapping("/security/token")
+public class TokenController {
+
+    @Autowired
+    @Qualifier(value = "tokenizedGeoUserDao")
+    private GeoUserDao geoUserDao;
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    @Transactional(readOnly = true)
+    public String getToken(HttpServletResponse response) throws IOException {
+        String token = geoUserDao.getCurrentGeoUser().getToken();
+        if (token == null){
+            response.setStatus(401);
+            response.getWriter().write("Access denied");
+            return null;
+        } else
+            return token;
+    }
+
+    @RequestMapping(value = "/generate", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    @Transactional
+    public String generateToken(HttpServletResponse response) throws IOException {
+        GeoUser geoUser = geoUserDao.getCurrentGeoUser();
+        if (geoUser == null){
+            response.setStatus(401);
+            response.getWriter().write("Access denied");
+            return null;
+        } else {
+            String newToken = UUID.randomUUID().toString();
+            geoUser.setToken(newToken);
+            geoUserDao.update(geoUser);
+
+            return newToken;
+        }
+    }
+}
