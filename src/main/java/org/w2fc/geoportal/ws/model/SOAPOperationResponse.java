c44844855fef22bd57beab90f89d4d369550e72e
EvgenLozov <jmen7070@gmail.com>
2016-02-24 08:24:54 +0200

    added xml annotation to soap response

--- src/main/java/org/w2fc/geoportal/ws/model/SOAPOperationResponse.java
+++ src/main/java/org/w2fc/geoportal/ws/model/SOAPOperationResponse.java
@@ -2,12 +2,22 @@ package org.w2fc.geoportal.ws.model;
 
 import org.w2fc.geoportal.reports.Report1Controller;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
 /**
  * @author yevhenlozov
  */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(namespace = "http://ws.portal.maps.yarcloud.ru/object/SOAPOperationResponse")
 public class SOAPOperationResponse {
 
+    @XmlElement(name = "pid", required=true)
     private String pid;
+
+    @XmlElement(name = "reportLink", required=true)
     private String reportLink;
 
     public SOAPOperationResponse() {
@@ -15,7 +25,7 @@ public class SOAPOperationResponse {
 
     public SOAPOperationResponse(String pid) {
         this.pid = pid;
-        this.reportLink = Report1Controller.REPORT_URL_PATTERN.replace("{pid}", pid);
+        this.reportLink = "/report" + Report1Controller.REPORT_URL_PATTERN.replace("{pid}", pid);
     }
 
     public String getPid() {
