c1515c033c6c80b8b615e2c0b45ac797bd15ffda
Oleg Oginskyi <oor1@ukr.net>
2016-03-29 13:31:08 +0300

    fixed a bug

--- src/main/java/org/w2fc/geoportal/ws/error/ErrorDesc.java
+++ src/main/java/org/w2fc/geoportal/ws/error/ErrorDesc.java
@@ -1,8 +1,17 @@
 package org.w2fc.geoportal.ws.error;
 
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class ErrorDesc {
 
+    @XmlElement
     int code;
+
+    @XmlElement
     String guid;
 
     public ErrorDesc(int code, String guid) {
--- src/main/java/org/w2fc/geoportal/ws/error/ErrorsReport.java
+++ src/main/java/org/w2fc/geoportal/ws/error/ErrorsReport.java
@@ -1,9 +1,14 @@
 package org.w2fc.geoportal.ws.error;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import java.util.List;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 public class ErrorsReport {
 
+    @XmlElement
     private List<ErrorDesc> errors;
 
     public ErrorsReport(List<ErrorDesc> errors) {
